a0d1b12ebb10d3444a3afe526b31a73e51f42373
Sergio Renato Aldana Alvarez <sergio.aldana@galileo.edu>
2021-10-30 09:57:41 -0600

    correción nombre ruta index

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'verified', 'role:super-admin'])->group(function () {
 
-    Route::get('/', App\Http\Livewire\Admin\Index::class)->name('index');;
+    Route::get('/', App\Http\Livewire\Admin\Index::class)->name('admin');;
 
     Route::get('/usuarios', App\Http\Livewire\Admin\Usuarios::class)->name('usuarios');
 });
--- routes/estudiante.php
+++ routes/estudiante.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'verified', 'role:estudiante'])
     ->get('', App\Http\Livewire\Estudiante\Index::class)
-    ->name('index');
+    ->name('estudiante');
 
 Route::middleware(['auth:sanctum', 'verified', 'role:estudiante'])
     ->get('/incorporacion/create', App\Http\Livewire\Estudiante\Index::class)
