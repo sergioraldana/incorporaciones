3859a5e2bd1b19f47e4e880747aed2d434512cf7
Sergio Renato Aldana Alvarez <sergio.aldana@galileo.edu>
2021-10-30 09:18:23 -0600

    Inicio Modelo Solicitud

---
+++ database/migrations/2021_10_17_182359_create_solicitud_table.php
@@ -0,0 +1,96 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+USE App\Models\Solicitud;
+
+class CreateSolicitudTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicitud', function (Blueprint $table) {
+            $table->id();
+            $table->enum('tipo_tramite', [Solicitud::INCORPORACION,
+                Solicitud::AUT_EJERCER_PROF_USAC,
+                Solicitud::REC_POSTGRADO_USAC,
+                Solicitud::REC_ESP_MEDICA_USAC,
+                Solicitud::REC_PROFESORES_USAC,
+                Solicitud::REGISTRO_DIPLOMA_USAC]);
+            //Datos Generales
+            $table->unsignedBigInteger('estudiante_usuario_id');
+            $table->foreignId('estudiante_usuario_id')->constrained('users');
+
+            $table->unsignedBigInteger('admin_usuario_id');
+            $table->foreignId('admin_usuario_id')->constrained('users');
+
+            $table->char('cui', 13);
+            $table->char('pasaporte', 20)->nullable();
+            $table->string('nombres', 100);
+            $table->string('apellidos', 100);
+            $table->char('telefono', 8)->nullable();
+            $table->char('celular', 8);
+            $table->string('correo', 50);
+
+            $table->unsignedBigInteger('dir_departamento_id');
+            $table->foreignId('dir_departamento_id')->constrained('departamento');
+
+            $table->unsignedBigInteger('dir_municipio_id');
+            $table->foreignId('dir_municipio_id')->constrained('municipio');
+
+            $table->string('direccion', 100);
+
+            $table->unsignedBigInteger('nac_departamento_id');
+            $table->foreignId('nac_departamento_id')->constrained('departamento');
+
+            $table->unsignedBigInteger('nac_municipio_id');
+            $table->foreignId('nac_municipio_id')->constrained('municipio');
+
+            $table->date('nac_fecha');
+
+            $table->unsignedBigInteger('nac_pais_id');
+            $table->foreignId('nac_pais_id')->constrained('pais');
+
+            //Estudios Realizados en el extranjero
+            $table->unsignedBigInteger('institucion_pais_id');
+            $table->foreignId('institucion_pais_id')->constrained('pais');
+
+            $table->string('institucion_graduacion', 100);
+            $table->string('titulo_profesional', 200);
+            $table->double('duracion', 1, 1);
+            $table->tinyInteger('cursos_aprobados');
+            $table->string('observaciones', 300)->nullable();
+
+            //Reconocimientos
+            $table->string('titulo_prof_grado', 100);
+            $table->smallInteger('numero_registro');
+            $table->date('fecha_registro');
+
+            //Opciones incorporacion
+            $table->enum('opcion_incorporacion', [Solicitud::EXAMEN, [Solicitud::SERVICIO]])->nullable();
+
+            
+            $table->index(['nombres']);
+            $table->index(['apellidos']);
+            $table->index(['cui']);
+            $table->index(['nac_pais_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicituds');
+    }
+}
---
+++ database/migrations/2021_10_17_222613_create_incorporacion_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Incorporacion;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncorporacionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incorporacion', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('solicitud_id');
+            $table->foreignId('solicitud_id')->constrained('solicitud');
+            $table->enum('opcion_incorporacion', [Incorporacion::EXAMEN, [Incorporacion::SERVICIO]]);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incorporacion');
+    }
+}
